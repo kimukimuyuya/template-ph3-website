6d5c4e98d53f742fb6956e946d3b4fa73bbf0729
Yuya Kimura <yuya1508272003@gmail.com>
2023-06-09 12:57:56 +0900

    quizページ完成

---
+++ src/app/Http/Controllers/QuizController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Question;
+
+class QuizController extends Controller
+{
+    //
+
+    public function index()
+    {
+        $questions = Question::all();
+        return view('quiz', compact('questions'));
+    }
+}
